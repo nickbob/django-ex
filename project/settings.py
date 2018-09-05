f251837b47973fc665e0ca655e01850414394fd5
nickbob <nickbobsa@gmail.com>
2018-09-05 15:36:36 +0300

    first experemental commit

--- project/settings.py
+++ project/settings.py
@@ -131,3 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 INTERNAL_IPS = ['127.0.0.1']
+#for test commit
